a887d103a1c536dad9a23e773fc19fdaea56aef8
Michał Szymański <smiszym@gmail.com>
2020-01-03 12:30:15 +0100

    Add route for accessing minimized pages

--- contentacc/backend.py
+++ contentacc/backend.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 # If `entrypoint` is not defined in app.yaml, App Engine will look for an app
 # called `app` in `main.py`.
@@ -11,3 +11,9 @@ def hello():
     return render_template(
         'index.html',
         generation_timestamp=str(datetime.now()))
+
+
+@app.route('/minimized-page', methods=['GET'])
+def minimized_page():
+    url = request.args.get('url')
+    return f"Not implemented. Tried to access {url}", 501
