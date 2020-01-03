34df950e4afac57a74e29c47a0965ebd7bec4879
Michał Szymański <smiszym@gmail.com>
2020-01-03 14:53:00 +0100

    Implement content extraction

--- contentacc/backend.py
+++ contentacc/backend.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 from flask import Flask, render_template, request
+from contentacc.content_extraction import extract_content_from_url
 
 # If `entrypoint` is not defined in app.yaml, App Engine will look for an app
 # called `app` in `main.py`.
@@ -16,4 +17,7 @@ def hello():
 @app.route('/minimized-page', methods=['GET'])
 def minimized_page():
     url = request.args.get('url')
-    return f"Not implemented. Tried to access {url}", 501
+    return render_template(
+        'minimized-page.html',
+        url=url,
+        content=extract_content_from_url(url))
---
+++ contentacc/content_extraction.py
@@ -0,0 +1,34 @@
+from bs4 import BeautifulSoup
+from collections import namedtuple
+import requests
+
+
+ExtractedContent = namedtuple(
+    'ExtractedContent',
+    'title text image_urls links')
+
+
+def div_class_paragraph_extractor(soup, cls):
+    result = []
+    for match in soup.find_all(class_=cls):
+        paragraphs = match.find_all('p')
+        if len(paragraphs) > 0:
+            for paragraph in paragraphs:
+                result.append(paragraph.get_text())
+        else:
+            result.append(match.get_text())
+    return result
+
+
+def extract_content_from_url(url):
+    r = requests.get(url)
+    if r.status_code != 200:
+        return
+    soup = BeautifulSoup(r.text, 'html.parser')
+    extracted_paragraphs = div_class_paragraph_extractor(
+        soup, ['article--text', 'articleBody', 'art_content'])
+    return ExtractedContent(
+        title=soup.title.string,
+        text=extracted_paragraphs,
+        image_urls=[img.get('src') for img in soup.find_all('img')],
+        links=[link.get('href') for link in soup.find_all('a')])
