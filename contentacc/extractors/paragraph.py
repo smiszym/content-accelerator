cc11fe72c30e2e30d37a7c2721fd3726f76529c5
Michał Szymański <smiszym@gmail.com>
2020-03-21 16:49:40 +0100

    Rewrite paragraph extractors

--- contentacc/extractors/paragraph.py
+++ contentacc/extractors/paragraph.py
@@ -1,18 +1,19 @@
+def html_element_paragraph_extractor(html_element):
+    paragraphs = html_element.find_all('p')
+    if len(paragraphs) > 0:
+        for paragraph in paragraphs:
+            yield paragraph.get_text()
+    else:
+        yield html_element.get_text()
+
+
 def div_class_paragraph_extractor(soup, cls):
     for match in soup.find_all(class_=cls):
-        paragraphs = match.find_all('p')
-        if len(paragraphs) > 0:
-            for paragraph in paragraphs:
-                yield paragraph.get_text()
-        else:
-            yield match.get_text()
+        for paragraph in html_element_paragraph_extractor(match):
+            yield paragraph
 
 
 def div_id_paragraph_extractor(soup, id):
     for match in soup.find_all(id=id):
-        paragraphs = match.find_all('p')
-        if len(paragraphs) > 0:
-            for paragraph in paragraphs:
-                yield paragraph.get_text()
-        else:
-            yield match.get_text()
+        for paragraph in html_element_paragraph_extractor(match):
+            yield paragraph
