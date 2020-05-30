becbe7565350ba8626f3ddf23b880b8f79db0b1e
Michał Szymański <smiszym@gmail.com>
2020-05-30 15:03:50 +0200

    Remove serving the frontend page from backend server

--- contentacc/backend.py
+++ contentacc/backend.py
@@ -1,19 +1,9 @@
-from datetime import datetime
-from flask import Flask, render_template, request
+from flask import Flask, request
 from contentacc.content_extraction import extract_content_from_url
 
 # If `entrypoint` is not defined in app.yaml, App Engine will look for an app
 # called `app` in `main.py`.
 app = Flask(__name__)
-
-
-@app.route('/', methods=['GET'])
-def index():
-    url = request.args.get('url')
-    return render_template(
-        'index.html',
-        url=url,
-        content=extract_content_from_url(url) if url is not None else None)
 
 
 @app.route('/minimized-page', methods=['GET'])
