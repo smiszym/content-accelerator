aece5712a08fa424e65bf5cefd295a69987184f8
Michał Szymański <smiszym@gmail.com>
2020-06-17 17:38:31 +0200

    Translate loading state messages into Polish

--- front/src/LoadingStateIndicator.js
+++ front/src/LoadingStateIndicator.js
@@ -4,9 +4,9 @@ export class LoadingStateIndicator extends Component {
   render() {
     if (this.props.loadingState && this.props.loadingState !== 'none') {
       const mapping = {
-        'none': 'The page you requested is being displayed',
-        'fetch': 'The page you requested is being fetched from the server',
-        'ready': 'The page you requested is ready to be displayed',
+        'none': 'Wybrana strona jest wyświetlana',
+        'fetch': 'Wybrana strona jest właśnie pobierana z serwera',
+        'ready': 'Wybrana strona jest gotowa do wyświetlenia',
       };
       return <div id="loading-state-indicator"><strong>{mapping[this.props.loadingState]}</strong></div>;
     } else {
