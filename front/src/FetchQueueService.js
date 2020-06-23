ff01d7459f7d1ac6ede1919903be1c0aee33616d
Michał Szymański <smiszym@gmail.com>
2020-06-23 15:58:42 +0200

    Implement FetchQueueService.insertWithPriority()

--- front/src/FetchQueueService.js
+++ front/src/FetchQueueService.js
@@ -1,6 +1,11 @@
 var Deque = require("double-ended-queue");
 
 export const FetchQueueService = {
+  insertWithPriority(url) {
+    // Called when the user tries to access a URL that is not yet in cache;
+    // it will be fetched with highest priority.
+    this.queue.insertFront(url);
+  },
   enqueue: function(url) {
     this.queue.enqueue(url);
   },
