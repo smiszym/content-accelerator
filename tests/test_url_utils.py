975ea26751db3fe9edb2ed3d9e2e6f9345aa54f9
Michał Szymański <smiszym@gmail.com>
2020-08-07 19:32:01 +0200

    Simplify the existing test

--- tests/test_url_utils.py
+++ tests/test_url_utils.py
@@ -1,11 +1,8 @@
-from unittest import TestCase
-
 from contentacc.url_utils import supply_scheme_and_netloc
 
 
-class TestSupplySchemeAndNetloc(TestCase):
-    def test_supply_scheme_and_netloc(self):
-        assert (supply_scheme_and_netloc(
-            '/wiki/Polska',
-            'https://pl.wikipedia.org/wiki/Jaszczurka_zwinka')
-                == 'https://pl.wikipedia.org/wiki/Polska')
+def test_supply_scheme_and_netloc():
+    assert (supply_scheme_and_netloc(
+        '/wiki/Polska',
+        'https://pl.wikipedia.org/wiki/Jaszczurka_zwinka')
+            == 'https://pl.wikipedia.org/wiki/Polska')
