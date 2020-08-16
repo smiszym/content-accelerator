994b99b80cdba5ecdbfb9bdd4ebfd02e6a3e3ec5
Michał Szymański <smiszym@gmail.com>
2020-08-16 14:36:15 +0200

    Simplify MainPage component implementation

--- front/src/MainPage.js
+++ front/src/MainPage.js
@@ -4,37 +4,25 @@ import { ContentView } from "./ContentView";
 import { LoadingStateIndicator } from "./LoadingStateIndicator";
 import { NewUrlPrompt } from "./NewUrlPrompt";
 
-class MainPageWithoutContent extends Component {
-  render() {
-    return <div>
-      <LoadingStateIndicator loadingState={this.props.loadingState} />
-      <h1 id="content-title">Content Accelerator</h1>
-      <p>
-        To jest aplikacja wspomagająca dostęp do treści internetowych w warunkach
-        słabego łącza.
-      </p>
-      <NewUrlPrompt loadPageFromUrl={this.props.loadPageFromUrl} />
-      <AvailableSpaceView />
-      {
-        this.props.lastPageLoadTime &&
-          <div>Ostatni artykuł załadowano w {this.props.lastPageLoadTime.toFixed(0)} ms</div>
-      }
-    </div>;
-  }
-}
-
-class MainPageWithContent extends Component {
+export class MainPage extends Component {
   render() {
     const content = this.props.content;
 
     return <div>
       <LoadingStateIndicator loadingState={this.props.loadingState} />
-      <h1 id="content-title">{content.title}</h1>
-      <ContentView
-        content={this.props.content}
-        loadPageFromUrl={this.props.loadPageFromUrl} />
+      <h1 id="content-title">{content ? content.title : "Content Accelerator"}</h1>
       {
-        content.links.length > 0 &&
+        content
+          ? <ContentView
+              content={this.props.content}
+              loadPageFromUrl={this.props.loadPageFromUrl} />
+          : <p>
+              To jest aplikacja wspomagająca dostęp do treści internetowych w warunkach
+              słabego łącza.
+            </p>
+      }
+      {
+        content && content.links.length > 0 &&
           <div>
             Linki:
             <ol>
@@ -58,21 +46,5 @@ class MainPageWithContent extends Component {
           <div>Ostatni artykuł załadowano w {this.props.lastPageLoadTime.toFixed(0)} ms</div>
       }
     </div>;
-  }
-}
-
-export class MainPage extends Component {
-  render() {
-    return this.props.content
-      ? <MainPageWithContent
-          loadingState={this.props.loadingState}
-          lastPageLoadTime={this.props.lastPageLoadTime}
-          content={this.props.content}
-          url={this.props.url}
-          loadPageFromUrl={this.props.loadPageFromUrl} />
-      : <MainPageWithoutContent
-          loadingState={this.props.loadingState}
-          lastPageLoadTime={this.props.lastPageLoadTime}
-          loadPageFromUrl={this.props.loadPageFromUrl} />;
   }
 }
